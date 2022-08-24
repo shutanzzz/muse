cb6ecdddbd04d149016d50579ecfda53b004a776
shutanzzz <45780284+shutanzzz@users.noreply.github.com>
2022-08-24 11:00:41 +0800

    Add files via upload

---
+++ src/demo.py
@@ -0,0 +1,60 @@
+import io
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+def load_vec(emb_path, nmax=50000):
+    vectors = []
+    word2id = {}
+    with io.open(emb_path, 'r', encoding='utf-8', newline='\n', errors='ignore') as f:
+        next(f)
+        for i, line in enumerate(f):
+            word, vect = line.rstrip().split(' ', 1)
+            vect = np.fromstring(vect, sep=' ')
+            assert word not in word2id, 'word found twice'
+            vectors.append(vect)
+            word2id[word] = len(word2id)
+            if len(word2id) == nmax:
+                break
+    id2word = {v: k for k, v in word2id.items()}
+    embeddings = np.vstack(vectors)
+    return embeddings, id2word, word2id
+
+def get_nn(word, src_emb, src_id2word, tgt_emb, tgt_id2word, K=5):
+    word2id = {v: k for k, v in src_id2word.items()}
+    word_emb = src_emb[word2id[word]]
+    scores = (tgt_emb / np.linalg.norm(tgt_emb, 2, 1)[:, None]).dot(word_emb / np.linalg.norm(word_emb))
+    k_best = scores.argsort()[-K:][::-1]
+    for i, idx in enumerate(k_best):
+        return tgt_id2word[idx]
+
+def plot_similar_word(src_words, src_word2id, src_emb, tgt_words, tgt_word2id, tgt_emb, pca, zhfont):
+
+    Y = []
+    word_labels = []
+    for sw in src_words:
+        Y.append(src_emb[src_word2id[sw]])
+        word_labels.append(sw)
+    for tw in tgt_words:
+        Y.append(tgt_emb[tgt_word2id[tw]])
+        word_labels.append(tw)
+
+    # find tsne coords for 2 dimensions
+    Y = pca.transform(Y)
+    x_coords = Y[:, 0]
+    y_coords = Y[:, 1]
+
+    # display scatter plot
+    plt.figure(figsize=(10, 8), dpi=80)
+    plt.scatter(x_coords, y_coords, marker='x')
+
+    for k, (label, x, y) in enumerate(zip(word_labels, x_coords, y_coords)):
+        color = 'blue' if k < len(src_words) else 'red'  # src words in blue / tgt words in red
+        plt.annotate(label, xy=(x, y), xytext=(0, 0), textcoords='offset points', fontsize=19,
+                     color=color, weight='bold', fontproperties=zhfont)
+
+    plt.xlim(x_coords.min() - 0.2, x_coords.max() + 0.2)
+    plt.ylim(y_coords.min() - 0.2, y_coords.max() + 0.2)
+    plt.title('Visualization of the multilingual word embedding space')
+
+    plt.show()
