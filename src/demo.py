570c453aa21ec33b7a67ab8c3fc7dc03eca0c86f
shutanzzz <45780284+shutanzzz@users.noreply.github.com>
2022-09-19 22:01:46 +0800

    Update demo.py

--- src/demo.py
+++ src/demo.py
@@ -3,22 +3,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 
-def load_vec(emb_path, nmax=50000):
-    vectors = []
-    word2id = {}
-    with io.open(emb_path, 'r', encoding='utf-8', newline='\n', errors='ignore') as f:
-        next(f)
-        for i, line in enumerate(f):
-            word, vect = line.rstrip().split(' ', 1)
-            vect = np.fromstring(vect, sep=' ')
-            assert word not in word2id, 'word found twice'
-            vectors.append(vect)
-            word2id[word] = len(word2id)
-            if len(word2id) == nmax:
-                break
-    id2word = {v: k for k, v in word2id.items()}
-    embeddings = np.vstack(vectors)
-    return embeddings, id2word, word2id
+def load_vec(emb_path):
+  vectors = []
+  word2id = {}
+  with io.open(emb_path, 'r', encoding='utf-8', newline='\n', errors='ignore') as f:
+      next(f)
+      for i, line in enumerate(f):
+          word, vect = line.rstrip().split(' ', 1)
+          vect = np.fromstring(vect, sep=' ')
+          assert word not in word2id, 'word found twice'
+          vectors.append(vect)
+          word2id[word] = len(word2id)
+  id2word = {v: k for k, v in word2id.items()}
+  embeddings = torch.tensor(vectors)
+  return embeddings, id2word, word2id
 
 def get_nn(word, src_emb, src_id2word, tgt_emb, tgt_id2word, K=5):
     word2id = {v: k for k, v in src_id2word.items()}
